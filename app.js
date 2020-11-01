594b3b71451426e23e1533e7bd5477901b241728
JMJ Riyana <jeemariyana@gmail.com>
2020-11-01 11:46:05 +0530

    d

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const CSVToJSON = require("csvtojson");
-const JSONToCSV = require("json2csv");
+const JSONToCSV = require("json2csv").parse;
 const FileSystem = require("fs");
+
+/*CSVToJSON().fromFile("./NAT64_1_20.csv").then(source => {
+    console.log(source);
+    source.push({
+
+    });
+    const csv = JSONToCSV(source, {fields: ["No.", "Time", "Source", "Destination", "Protocol", "Length", "Info"]});
+    FileSystem.writeFileSync("./destination.csv",csv);
+});*/
 
 CSVToJSON().fromFile("./NAT64_1_20.csv").then(source => {
     console.log(source);
     source.push({
 
+    });
+    //const csv = JSONToCSV(source, {fields: ["No.", "Time", "Source", "Destination", "Protocol", "Length", "Info"]});
+    FileSystem.writeFile("./source.json",JSON.stringify(source, null), (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON array is saved.");
     });
 });
