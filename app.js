8bd62ef2691af6bac0cc00b60a508f8a332a86ff
JMJ Riyana <jeemariyana@gmail.com>
2020-11-01 10:56:58 +0530

    d

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const CSVToJSON = require("csvtojson");
 const JSONToCSV = require("json2csv");
+const FileSystem = require("fs");
+
+CSVToJSON().fromFile("./NAT64_1_20.csv").then(source => {
+    console.log(source);
+    source.push({
+
+    });
+});
