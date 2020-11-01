4853887c1546ce14fa45e64db50c10196042b179
JMJ Riyana <jeemariyana@gmail.com>
2020-11-01 16:12:38 +0530

    d

--- app.js
+++ app.js
@@ -48,9 +48,12 @@ const FileSystem = require("fs");
 })();*/
 
 // read JSON from a file
+//******* */ convert JSON array to CSV string
+
+/*
 const source = JSON.parse(FileSystem.readFileSync('users.json'));
 
-//******* */ convert JSON array to CSV string
+
 (async () => {
     try {
         const csv = JSONToCSV(source);
@@ -65,3 +68,51 @@ const source = JSON.parse(FileSystem.readFileSync('users.json'));
         console.log(err);
     }
 })();
+*/import {saveAs} from 'file-saver';
+
+function createCsv() {
+    let csv = ''
+    let row = ''
+    let headers = ''
+    headers = headers.concat('collection', ',', 'collection_uid', ',', 'survey', ',', 'survey_uid', ',', 'question', ',', 'answer')
+    csv = csv.concat(headers)
+    data.forEach(answer => {
+        row = ''
+        // you need to start your row with \n such that excel would know that is the new line 
+        // you can use other delimiters of course, I found this one to be easily picked up by Excel
+        row = row.concat("\n",
+            your.data.goes.here, ",",
+            your.data.goes.here, ",",
+            answer
+        )
+        csv = csv.concat(row)
+    })
+
+    // create the blob declare it as csv
+    const textBlob = new Blob([csv], {type: 'application/csv'})
+    //you don't really need this 
+    const csvFile = new File([textBlob], "users.csv", {
+        contentType: "application/csv"
+    });
+    // this sets state for the component 
+    setCsvForExportFileURL(textBlob)
+    // this sets state variable that displays the button after csv has been created
+    setShouldShowDownloadButton(true)
+}
+
+
+
+
+//in your render method you will have something like
+{
+    shouldShowDownloadButton && // display the button
+    csvForExportFileURL && // and the url has been created
+    <Button color={'primary'} variant={"contained"}
+            onClick={saveAsCsv.bind(saveAsCsv)} // go to saveAsCsv method
+            className={classes.button}>download answers</Button>
+}
+
+// you could have done this in line above but I find this neater
+function saveAsCsv() {
+    saveAs(csvForExportFileURL, 'ella_all_answers.csv')
+}
