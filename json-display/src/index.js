9faae843598abb49f2e4c07ba98982333a7385a0
JMJ Riyana <jeemariyana@gmail.com>
2020-11-02 14:20:40 +0530

    d

--- json-display/src/index.js
+++ json-display/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
+import "./style.css";
 import Table from './table';
 
 function App() {
--- json-display/src/table.js
+++ json-display/src/table.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import React from "react";
+
+const data = require("./data");
 
 const Table = () => {
-    return <p>Table</p>;
+  const dataImport = data[0];
+
+  function renderHeaderCells() {
+    let headerCells = [];
+
+    Object.keys(dataImport).map((x, i) => {
+      let items = Object.values(dataImport)[i];
+      headerCells.push(
+        <th colSpan={Object.keys(items).length} key={i.name}>
+          {x}
+        </th>
+      );
+      return headerCells;
+    });
+    return headerCells;
+  }
+
+  /*function renderSubHeaders() {
+    let subHeaders = [];
+    let subs = Object.values(dataImport);
+
+    subs.map((x, i) => {
+      if (subs[i] !== undefined) {
+        Object.keys(subs[i]).map(y => {
+          subHeaders.push(<td>{y}</td>);
+          return subHeaders;
+        });
+      }
+      return subHeaders;
+    });
+    return subHeaders;
+  }*/
+
+  function renderResults() {
+    let results = [];
+    let res = Object.values(dataImport);
+
+    res.map((x, i) => {
+      if (res[i] !== undefined) {
+        Object.values(res[i]).map(y => {
+          results.push(<td>{y}</td>);
+          return results;
+        });
+      }
+      return results;
+    });
+
+    return results;
+  }
+
+  return (
+    <React.Fragment>
+      <table>
+        <thead>
+          <tr>{renderHeaderCells()}</tr>
+        </thead>
+        <tbody>
+          <tr>{renderResults()}</tr>
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
 };
 
 export default Table;
