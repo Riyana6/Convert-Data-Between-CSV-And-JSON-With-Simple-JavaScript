4c483b74e6622a895bd37f797c4dc2f94e31dc73
JMJ Riyana <jeemariyana@gmail.com>
2020-11-11 19:40:17 +0530

    d

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-/*const JSONToCSV = require("json2csv").parse;
+const JSONToCSV = require("json2csv").parse;
 const FileSystem = require("fs");
-const source = JSON.parse(FileSystem.readFileSync('user.json'));
+const source = JSON.parse(FileSystem.readFileSync('data.json'));
 (async () => {
     try {
         const csv = JSONToCSV(source);
 
         // print CSV string
-        console.log(csv);
+        //console.log(csv);
 
         // write CSV to a file
-        FileSystem.writeFileSync('newuser.csv', csv);
+        FileSystem.writeFileSync('data.csv', csv);
 
     } catch (err) {
         console.log(err);
         
     }
-})();*/
-
+})();
+/*
 const CSVToJSON = require("csvtojson");
 const FileSystem = require("fs");
 
 (async () => {
     try {
-        const users = await CSVToJSON().fromFile('newuser.csv');
+        const users = await CSVToJSON().fromFile('NAT64_1_20.csv');
 
         // log the JSON array
-        console.log(users);
+       // console.log(users);
 
         //create json file
-        FileSystem.writeFile('newuser.json', JSON.stringify(users, null, 4), (err) => {
+        FileSystem.writeFile('data.json', JSON.stringify(users, null, 4), (err) => {
             if (err) {
                 throw err;
             }
@@ -39,4 +39,4 @@ const FileSystem = require("fs");
         console.log(err);
     }
     
-})()
+})();*/
